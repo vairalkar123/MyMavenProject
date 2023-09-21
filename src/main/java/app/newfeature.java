788b3c91e0f374846dc8894980b9826c621782e4
vairalkar123 <vairalkarsagar01@gmail.com>
2023-09-21 12:11:34 +0530

    some extra code

--- src/main/java/app/newfeature.java
+++ src/main/java/app/newfeature.java
@@ -4,5 +4,7 @@ public class newfeature {
 	public static void main(String []args) {
 		System.out.println("this is new branch");
 	}
-
+	public static int add() {
+	 System.out.println("this is new branch");
+	return 0;}
 }
