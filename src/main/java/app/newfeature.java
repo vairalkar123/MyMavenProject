08fc5f3f13f2523baca551e6acefffe5c0d09ddd
vairalkar123 <vairalkarsagar01@gmail.com>
2023-09-21 15:07:49 +0530

    one method

--- src/main/java/app/newfeature.java
+++ src/main/java/app/newfeature.java
@@ -1,8 +1,13 @@
 package app;
 
 public class newfeature {
+	public static void one() {
+		System.out.println("this is code for one");
+	}
+	
 	public static void main(String []args) {
 		System.out.println("this is new branch");
+		one();
 	}
 	public static int add() {
 	 System.out.println("this is new branch");
